a544bb8c28aa2a419ac11725f893e86a0dc7c7fb
idnyaneshwaraxioned <dnyaneshwari@prdxn.com>
2021-06-28 20:05:39 +0530

    ModifyPrmiseAPI component

--- src/component/Promiseapi.js
+++ src/component/Promiseapi.js
@@ -13,7 +13,7 @@ const Promiseapi = () => {
 				value.id<5?
 					fetch(`https://jsonplaceholder.typicode.com/users/${value.id}`)
 						.then(response => response.json())
-						.then(ele => setUser(...user,ele))
+						.then(ele => setUser([ele]))
 						.catch(err => console.log(err)):null
 				)
 			).catch(err => console.log(err));
@@ -25,7 +25,7 @@ const Promiseapi = () => {
 		response.map(async (user) => {
 			const getUser = await fetch(`https://jsonplaceholder.typicode.com/users/${user.id}`)
 			const getUserResponse = await getUser.json();
-			getUserResponse.id<5 && setUser(getUserResponse);
+			getUserResponse.id<5 && setUser([getUserResponse]);
 		})
 	}
 
@@ -34,14 +34,14 @@ const Promiseapi = () => {
 			<button onClick={promiseHandler}>Promise	</button>
 			<button onClick={promiseAsync}>Async/Await	</button>
 			{
-				// user.map(value =>
-				<ul >
-					<li>ID: {user.id}</li>
-					<li>Name: {user.name}</li>
-					<li>Username: {user.username}</li>
-					<li>Email: {user.email}</li>
+				user.map(value =>
+				<ul key={value.id}>
+					<li>ID: {value.id}</li>
+					<li>Name: {value.name}</li>
+					<li>Username: {value.username}</li>
+					<li>Email: {value.email}</li>
 				</ul>
-				// )
+				)
 			}
 		</div>
 	)
